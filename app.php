32c81ab3f7ec7a34a40f29aeb46a7f4902cab5ba
Tomiwa-Ot <ot.server1@outlook.com>
2023-05-15 20:54:51 +0100

    made changes

--- app.php
+++ app.php
@@ -10,28 +10,5 @@ $route->post('/v1/login', function() {
     Login::get();
 });
 
-$route->post('/v1/register', function() {
-    Register::get();
-});
-
-$route->get('/v1/transactions', function() {
-    Wallet::getTransactions();
-});
-
-$route->post('/v1/deposit', function() {
-    Wallet::deposit();
-});
-
-$route->post('/v1/withdraw', function() {
-    Wallet::withdraw();
-});
-
-$route->post('/v1/transfer', function() {
-    Wallet::transfer();
-});
-
-$route->get('/v1/balance', function() {
-    Wallet::getBalance();
-});
 
 $route->submit();
